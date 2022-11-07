575d6e18496fe6c6f2f3d5f0272bc0062eb1d938
Alessio Garibaldi <alessiogaribaldi2000@gmail.com>
2022-11-07 14:36:56 +0100

    added elements' style (with Javascript)

---
+++ js/elements-style.js
@@ -0,0 +1,40 @@
+numberBox.style.cssText = `width: 100px; 
+display: flex;
+align-items: center;
+justify-content: center;
+font-size: 2.5rem;
+font-weight: bold;
+background-color: #ffe997ea;
+color: #743d09;
+border: 5px solid #ff6200;
+border-radius: 15px; `
+
+buttonsBox.style.cssText = `width: 250px;
+display: flex;
+justify-content: space-between;
+padding: 20px;
+border-radius: 15px;
+border: 5px solid #ff6200;
+background-color: #ffe997de;`
+
+decrease.style.cssText = ` width: 50px;
+cursor: pointer;
+font-size: 1.5rem;
+color: #743d09;
+display: flex;
+align-items: center;
+justify-content: center;
+border-radius: 10px;
+border: 3px solid #743d09;
+background-color: #ff9500;`
+
+increase.style.cssText = `width: 50px;
+cursor: pointer;
+font-size: 1.5rem;
+color: #743d09;
+display: flex;
+align-items: center;
+justify-content: center;
+border-radius: 10px;
+border: 3px solid #743d09;
+background-color: #ff9500;`
