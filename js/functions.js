ce02f348398f4fc2fcff8e76c2700e4329fb121c
Alessio Garibaldi <alessiogaribaldi2000@gmail.com>
2022-11-07 14:41:34 +0100

    added functions (javascript)

---
+++ js/functions.js
@@ -0,0 +1,12 @@
+increase.addEventListener("click", function add() {
+    let result = Number(numberBox.innerText) + 1;
+    numberBox.innerText = result;
+});
+
+
+decrease.addEventListener("click", function subtract() {
+    if (numberBox.innerText != 0) {
+        let result = Number(numberBox.innerText) - 1;
+        numberBox.innerText = result;
+    }
+});
