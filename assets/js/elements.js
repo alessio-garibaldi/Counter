0a4cf1162f77fef37af43ab89545c13847dc56de
Alessio Garibaldi <alessiogaribaldi2000@gmail.com>
2022-11-09 00:22:49 +0100

    creazione elementi (funzione)
    (after first correction feedback)

--- assets/js/elements.js
+++ assets/js/elements.js
@@ -1,25 +1,21 @@
 let counter = document.querySelector("#counter");  // VARIABLE FOR THE COUNTER ELEMENT
 
-// CREATION OF MY INITIAL VALUE (0)
-let numberBox = document.createElement("div");
-numberBox.classList.add("number-box")
-counter.appendChild(numberBox) 
-numberBox.innerText = 0  // INITIAL VALUE
+function createElement(tag, text, classname){
+    var element = document.createElement(tag);
+    element.innerHTML = text;
+    element.classList.add(classname);
+    return element
+}
 
+var numberBox = createElement("div", "0", "number-box");
+counter.appendChild(numberBox)
 
-// CREATION OF THE BUTTON'S CONTAINER
-let buttonsBox = document.createElement("div")
-buttonsBox.classList.add("buttons-box")
+console.log(numberBox)
+var buttonsBox = createElement("div", "", "buttons-box");
 counter.appendChild(buttonsBox)
 
-
-// CREATION OF MY BUTTONS ( - , + )
-let decrease = document.createElement("button")
-decrease.classList.add("decrease")
+var decrease = createElement("button", "-", "increase");
 buttonsBox.appendChild(decrease)
-decrease.innerHTML = "-"
 
-let increase = document.createElement("button")
-increase.classList.add("increase")
+var increase = createElement("button", "+", "decrease");
 buttonsBox.appendChild(increase)
-increase.innerHTML = "+"
